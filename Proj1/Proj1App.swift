6e8cb0395fb9c27bfb44ee7978f09af4904c57f4
Sota Takamura <miiko.dsoul@gmail.com>
2022-05-02 02:46:55 +0900

    Initial Commit

---
+++ Proj1/Proj1App.swift
@@ -0,0 +1,17 @@
+//
+//  Proj1App.swift
+//  Proj1
+//
+//  Created by Mi-ko Sounyan on 2022/05/02.
+//
+
+import SwiftUI
+
+@main
+struct Proj1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
